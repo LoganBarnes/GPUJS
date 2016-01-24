d509d43ec95e34cc9d1090bffce4f2fc82540940
scottmcgowan <scottmcgowan@email.arizona.edu>
2016-01-23 23:21:40 -0700

    script loading refactor

--- particles/js/main.js
+++ particles/js/main.js
@@ -1,8 +1,24 @@
+// On page load
+$(function() {
+    resetEditor();
+});
+
+// Submit editor text
 $('#submit-button').click(function(){
     var result = editor.getValue();
+    
+    //TODO: Do something with result
     console.log(result);
 });
 
-$('#reset-button').click(function(){
-    editor.setValue("Reset!");
-});
+// Reset editor text
+$('#reset-button').click(resetEditor);
+function resetEditor() {
+    var str = "function foo(items) {\n" +
+          "    var i;\n" +
+          "    for (i = 0; i < items.length; i++) {\n" +
+          "        alert(\"w00t!\" + items[i]);\n" +
+          "    }\n" +
+          "}";
+    editor.setValue(str);
+}
