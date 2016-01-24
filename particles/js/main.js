cef68bff21d96a31ebb0343b5ce9853a2dd7b446
scottmcgowan <scottmcgowan@email.arizona.edu>
2016-01-24 03:42:26 -0700

    editor work

--- particles/js/main.js
+++ particles/js/main.js
@@ -1,5 +1,10 @@
 // On page load
 $(function() {
+    // Load text editor
+    editor = ace.edit("particles-editor");
+    editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/glsl");
+
     resetEditor();
 });
 
@@ -44,6 +49,58 @@ function resetEditor() {
   
     $.get("res/user_sample_bouncers.txt", function(data) {
         editor.setValue(data);
+
+        // var session = editor.getSession();
+        // var Range = ace.require("ace/range").Range;
+
+        // var header = new Range(0, 0, 3, 0);
+        // var marker_header = session.addMarker(header, "static-text");
+        // console.log(header);
+
+        // var footer = new Range(5, 0, 5, 12);
+        // var marker_footer = session.addMarker(footer, "static-text");
+        // console.log(footer);
+        
+        // editor.keyBinding.addKeyboardHandler({
+        //     handleKeyboard : function(data, hash, keyString, keyCode, event) {
+        //         if (hash === -1 || (keyCode <= 40 && keyCode >= 37)) return false;
+        //         if (intersects(header) || intersects(footer)) {
+        //             return {command:"null", passEvent:false};
+        //         }
+        //     }
+        // });
+        
+        // before(editor, 'onPaste', preventReadonly);
+        // before(editor, 'onCut', preventReadonly);
+        
+        // header.start = session.doc.createAnchor(header.start);
+        // header.end = session.doc.createAnchor(header.end);
+        // header.end.$insertRight = true;
+        
+        // footer.start = session.doc.createAnchor(footer.start);
+        // footer.end = session.doc.createAnchor(footer.end);
+        // footer.end.$insertRight = true;
+        
+        // function before(obj, method, wrapper) {
+        //     var orig = obj[method];
+        //     obj[method] = function() {
+        //         var args = Array.prototype.slice.call(arguments);
+        //         return wrapper.apply(this, function(){
+        //             return orig.apply(obj, origArgs);
+        //         }, args);
+        //     }
+        //     return obj[method];
+        // }
+        
+        // function intersects(range) {
+        //     return editor.getSelectionRange().intersects(range);
+        // }
+        
+        // function preventReadonly(next) {
+        //     if (intersects(range)) return;
+        //     next();
+        // }
+
         editor.focus();
 
         var userSolver = "res/shaders/bouncers.frag";
@@ -53,3 +110,7 @@ function resetEditor() {
         particlesClass.tick();
     });
 }
+
+
+// Editor Config
+
